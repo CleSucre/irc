ab4ff766a9e6696fbe757466ce76d49e0eda1e40
Maxime <pierrot.maxime.pro@gmail.com>
2025-05-18 03:15:11 +0200

    docs(Client.cpp): Add doc upside functions Every functions have now theses own documentation, Some of them have comments inside to enlighte futur improvement

--- irc/src/client/Client.cpp
+++ irc/src/client/Client.cpp
@@ -18,23 +18,45 @@ Client::Client(Server *server, int fd) : _server(server), _fd(fd) {
 
 Client::~Client() {}
 
+/**
+ * @brief Get the file descriptor of the client
+ * 
+ * @return int : file descriptor
+ */
 int Client::getFd() const {
     return _fd;
 }
 
+/**
+ * @brief Set the NickName of the client
+ * 		  Do nothing if new NickName is empty
+ * @param name : NickName to set
+ * @return void
+ */
 void Client::setNick(const std::string &name)
 {
     if (name.length() < 1)
-        std::cerr << "Client " << _id.Username << " has entered a wrong NickName." << std::endl;
+		return ;
     //TODO: Check if new NickName isn't already taken in the server and in channels
 	if (_id.Nickname != name)
 		_id.Nickname = name;
 	std::cout << "Client " << _id.Username << " has entered a new NickName : " << _id.Nickname << std::endl;
 }
 
-
+/**
+ * @brief Set the UserName of the client
+ * 
+ * @param name : UserName to set
+ * @return true if ok, false if error
+ */
 bool Client::setUser(const std::string &name)
 {
+	if (name.length() < 1)
+	{
+		std::cerr << "Client " << _id.Username << " has entered a wrong UserName." << std::endl;
+		return (false);
+	}
+	//TODO: Check if new UserName isn't already taken in the server and in channels --- Same function as in Nickname
 	if (_id.certify == true)
 	{
 		std::cerr << "462 ERR_ALREADYREGISTERED :Unauthorized command (already registered) "<< std::endl;
@@ -42,7 +64,7 @@ bool Client::setUser(const std::string &name)
 	}
 	_id.Username = name;
 	std::cout << "Client " << _fd << " has is now known as : " << _id.Username << std::endl;
-    return true;
+    return (true);
 }
 
 
@@ -68,8 +90,8 @@ DataTracker IETF
 */
 
 /**
- * @brief Check if Client is already certyfied or not
- * 			if not, ask client to input an username and a nickname
+ * @brief Check if the client is identified and set Nickname and Username if needed
+ * 
  * @return false if error, true if ok
  */
 bool Client::checkIdentification()
@@ -92,7 +114,13 @@ bool Client::checkIdentification()
     std::cout << "id.certify : " << _id.certify << std::endl;
 	return true;
 }
-
+/**
+ * @brief Check if the command is valid
+ * @param arg : input command to check
+ * 
+ * @return true if command is valid
+ * @return false if error
+ */
 bool Client::go_command(std::string arg)
 {
     std::cout << "Here is arg [" << arg << "]" << std::endl;
@@ -117,10 +145,10 @@ bool Client::go_command(std::string arg)
  *        Read them on a temporary char[BUFFER_SIZE]
  * 		  If bytes > 0, stock them in client->buffer and search for a "\n" 
  * 
- * @return bool true if the client is still connected, false otherwise
+ * @return true if ok, false if error
  */
 bool Client::listen() {
-
+	// Listen for incoming messages from the client inside a buffer
     char buffer[BUFFER_SIZE];
     std::memset(buffer, 0, sizeof(buffer));
     int bytes_read = recv(_fd, buffer, sizeof(buffer) - 1, 0);
@@ -129,21 +157,27 @@ bool Client::listen() {
         return false;
     }
 	buffer[bytes_read] = '\0';
+	// Concatenate the buffer to the client's buffer
 	_buff.append(buffer);
     std::cout << "Here is buff : [" << _buff << "]\n\n" << std::endl; 
+	// Check identification
 	if (checkIdentification() == false)
     {
         _buff.erase(0, _buff.size());
         std::cerr << "out here" << std::endl;
 		return true;
     }
+	// Check if the buffer contains a complete command
     size_t pos;
 	std::cout << "Here is buff : [" << _buff << "]" << std::endl;
     while ((pos = _buff.find("\r\n")) != std::string::npos) {
 		std::cout << "pos : " << pos << std::endl;
         std::string arg = _buff.substr(0, pos);
         _buff.erase(0, pos + 2);
+		// Check if the command is valid
+		// TODO: Parse command and send it to person C
         go_command(arg);
+		// Valid packet received
         packetRecieption(*this, arg);
         
     }
