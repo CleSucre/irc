2d7fb502d3400dc4d4ceeaddc34fa1cfc9ed93c2
Maxime <pierrot.maxime.pro@gmail.com>
2025-06-02 20:02:23 +0200

    chore(Bot): add agv parse, rm comments

--- irc/bot_includes/Bot.hpp
+++ irc/bot_includes/Bot.hpp
@@ -1,9 +1,18 @@
 #ifndef BOT_HPP
 	#define BOT_HPP
 
+#include <cstdlib>
+#include <string>
+#include <ctime>
+#include <vector>
+
+
 class Bot
 {
 	private:
+		size_t current_channel;
+		std::vector<std::string, size_t> flood_timer;
+        std::vector<size_t, size_t> client_by_channel; 
 
 	public:
 		Bot();
--- irc/bot_src/main.cpp
+++ irc/bot_src/main.cpp
@@ -1,6 +1,5 @@
 #include "Bot.hpp"
 
-// main.cpp
 #include <iostream>
 #include <string>
 #include <sys/socket.h>
@@ -10,24 +9,27 @@
 #include <cstdlib>
 
 /**
- * @brief Main function to connect to an IRC server and respond to PING messages
+ * @brief Main function to connect to an IRC server
  * @param argc Number of command line arguments
  * @param argv Array of command line arguments
  * @return 0 on success, 1 on error
  */
 int main(int argc, char* argv[]) {
+
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <server> [port] [nick]\n";
+        return 1;
+    }
     const char* server = (argc > 1 ? argv[1] : "localhost");
     int port          = (argc > 2 ? std::atoi(argv[2]) : 6667);
     const char* nick   = (argc > 3 ? argv[3] : "bot");
 
-    // 2) Socket creation
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         std::cerr << "Socket Error\n";
         return 1;
     }
 
-    // 3) Server address setup
     sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
@@ -36,14 +38,12 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // 4) Server connection
     if (connect(sock, (sockaddr*)&addr, sizeof(addr)) < 0) {
         std::cerr << "Impossible de se connecter à " << server << ":" << port << "\n";
         return 1;
     }
     std::cout << "Connecté à " << server << ":" << port << "\n";
 
-    // 5) Send NICK and USER commands
     std::string nickCmd = "NICK " + std::string(nick) + "\r\n";
     std::string userCmd = "USER " + std::string(nick) + " 0 * :Server bot\r\n";
     std::cout << "NICK command sent\n" << std::endl;
@@ -52,7 +52,6 @@ int main(int argc, char* argv[]) {
     std::cout << "USER command sent\n" << std::endl;
     send(sock, userCmd.c_str(), userCmd.size(), 0);
 
-    // 6) Boucle de réception et réponse au PING
     char buffer[512];
     while (true) {
         std::cout << "En attente de messages...\n";
@@ -62,7 +61,7 @@ int main(int argc, char* argv[]) {
         std::cout << buffer;
 
         std::string line(buffer);
-        if (line.find("PING :") == 0) {
+        if (line.find("PRIVMSG :") == 0) {
             std::string token = line.substr(6);
             std::string pong = "PONG :" + token + "\r\n";
             send(sock, pong.c_str(), pong.size(), 0);
